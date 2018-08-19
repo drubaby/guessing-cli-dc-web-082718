50b1770917bceb14ca5d086e79b8cc34382efad8
drubaby <peryton6@gmail.com>
2018-08-19 14:34:28 -0400

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,13 +1,13 @@
 # Code your solution here!
 def run_guessing_game
-  while true
+  while true #will continue looping through program until exit break
   puts "Guess a number between 1 and 6."
   computer_guess = rand(1..6)
   human_guess = gets.chomp
 
 if human_guess == "exit"
   puts "Goodbye!"
-  break
+  break #ends the while loop
 elsif human_guess.to_i == computer_guess
   puts "You guessed the correct number!"
 else
